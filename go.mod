5122ae7428c438868c7e6a983fe4d4717e75f500
WaScript3r <wascript3r@gmail.com>
2021-05-25 14:28:32 +0300

    implemented socket pool

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/mailru/easygo v0.0.0-20190618140210-3c14a0dc985f
 	github.com/wascript3r/cryptopay v0.0.4
 	github.com/wascript3r/gopool v0.0.0-20200227213539-8fa9e794e69d
+	github.com/wascript3r/gostr v0.0.0-20200222144921-e78b2166c216
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 )
 
---
+++ pool/pool.go
@@ -0,0 +1,108 @@
+package pool
+
+import (
+	"context"
+	"encoding/json"
+	"sync"
+
+	"github.com/wascript3r/cryptopay/pkg/logger"
+	"github.com/wascript3r/gopool"
+	"github.com/wascript3r/gows"
+	"github.com/wascript3r/gows/router"
+)
+
+type Pool struct {
+	pool *gopool.Pool
+	log  logger.Usecase
+
+	mx        *sync.RWMutex
+	sockets   map[*gows.Socket]struct{}
+	writeJSON chan router.Params
+}
+
+func New(ctx context.Context, pool *gopool.Pool, log logger.Usecase, ev gows.EventBus) (*Pool, error) {
+	p := &Pool{
+		pool: pool,
+		log:  log,
+
+		mx:      &sync.RWMutex{},
+		sockets: make(map[*gows.Socket]struct{}),
+	}
+
+	ev.Subscribe(gows.NewConnectionEvent, p.handleNewConn)
+	ev.Subscribe(gows.DisconnectEvent, p.handleDisconnect)
+
+	return p, nil
+}
+
+func (p *Pool) start(ctx context.Context) error {
+	return p.pool.Schedule(func() {
+		var err error
+
+		for {
+			select {
+			case v := <-p.write:
+				err = p.writeAllJSON(v)
+				if err != nil {
+					p.log.Error("Cannot write message to all sockets because an error occurred: %s", err)
+				}
+
+			case <-ctx.Done():
+				p.log.Info("Stopped socket pool...")
+				return
+			}
+		}
+	})
+}
+
+func (p *Pool) handleNewConn(_ context.Context, socket *gows.Socket, req *gows.Request) {
+	p.mx.Lock()
+	defer p.mx.Unlock()
+
+	p.sockets[socket] = struct{}{}
+}
+
+func (p *Pool) handleDisconnect(_ context.Context, socket *gows.Socket, req *gows.Request) {
+	p.mx.Lock()
+	defer p.mx.Unlock()
+
+	delete(p.sockets, socket)
+}
+
+func (p *Pool) NumSockets() int {
+	p.mx.RLock()
+	defer p.mx.RUnlock()
+
+	return len(p.sockets)
+}
+
+func (p *Pool) writeAllJSON(v router.Params) error {
+	bs, err := json.Marshal(router.Response{
+		Err:    nil,
+		Params: v,
+	})
+	if err != nil {
+		return err
+	}
+
+	p.mx.RLock()
+	defer p.mx.Unlock()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(p.sockets))
+
+	for s, _ := range p.sockets {
+		s := s
+		p.pool.Schedule(func() {
+			s.Write(bs)
+			wg.Done()
+		})
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func (p *Pool) WriteJSONChan() chan<- router.Params {
+	return p.writeJSON
+}
