d8ef5627c65a9acf7e182ebdf66faba9a5a51614
WaScript3r <wascript3r@gmail.com>
2021-05-27 20:05:14 +0300

    added method param to router response

--- router/response.go
+++ router/response.go
@@ -9,29 +9,32 @@ type Params map[string]interface{}
 
 type Response struct {
 	Err    *string `json:"e"`
+	Method string  `json:"m"`
 	Params Params  `json:"p"`
 }
 
-func WriteErr(s *gows.Socket, e string) error {
-	e = gostr.UpperFirst(e)
+func WriteErr(s *gows.Socket, error, method string) error {
+	error = gostr.UpperFirst(error)
 
 	return s.WriteJSON(Response{
-		Err:    &e,
+		Err:    &error,
+		Method: method,
 		Params: nil,
 	})
 }
 
-func WriteBadRequest(s *gows.Socket) error {
-	return WriteErr(s, ErrBadRequest.Error())
+func WriteBadRequest(s *gows.Socket, method string) error {
+	return WriteErr(s, ErrBadRequest.Error(), method)
 }
 
-func WriteInternalError(s *gows.Socket) error {
-	return WriteErr(s, ErrInternalError.Error())
+func WriteInternalError(s *gows.Socket, method string) error {
+	return WriteErr(s, ErrInternalError.Error(), method)
 }
 
-func WriteRes(s *gows.Socket, p Params) error {
+func WriteRes(s *gows.Socket, method string, p Params) error {
 	return s.WriteJSON(Response{
 		Err:    nil,
+		Method: method,
 		Params: p,
 	})
 }
